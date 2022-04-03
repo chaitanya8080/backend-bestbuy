9a3f5531b9df9c0408b251e7e3814596c8763a96
chaitanya8080 <girase.chaitanya@gmail.com>
2022-04-03 21:09:02 +0530

    change3

--- src/config/googleauth.js
+++ src/config/googleauth.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
    
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:2222/auth/google/callback"
+    callbackURL: "https://backend-bestbuy.herokuapp.com/auth/google/callback"
   },
  
  async function(accessToken, refreshToken, profile, cb) {
